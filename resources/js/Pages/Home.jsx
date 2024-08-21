a511a54ace9b2ffea3085474c8e49363fa5871c0
Moliari <danielmoliaribarbosa@gmail.com>
2024-08-21 11:40:18 -0300

    feat: Configurações iniciais do projeto, usando Inertia.js, React, Bootstrap, com Php e Laravel no vite

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <>
+            <h1>Hello users</h1>
+            <button className="btn btn-primary">Click me!</button>
+        </>
+    );
+}

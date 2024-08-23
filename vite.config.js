71a9088a3348ee13e4e3f98cf7f973a4c7d058d1
Moliari <danielmoliaribarbosa@gmail.com>
2024-08-23 12:16:20 -0300

    fix: Configura caminha do build para resolver problema do blocked mixed content

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    base: "/build/", // Adicione a base URL para garantir que os ativos sejam carregados corretamente
     plugins: [
         laravel({
             input: ["resources/js/app.jsx"],

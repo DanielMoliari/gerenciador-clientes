3b429f47c6b966a777cae5723e351299dd0c41a6
Moliari <danielmoliaribarbosa@gmail.com>
2024-08-23 15:13:17 -0300

    fix: Ajuste no bootstrap, para que aceite só HTTPS indo para o site

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+window.axios.defaults.baseURL =
+    process.env.MIX_APP_URL ||
+    "https://gerenciador-clientes-daniel-ddcba2d51b3b.herokuapp.com/";

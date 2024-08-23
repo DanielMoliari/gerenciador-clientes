c29ac628c40ea854c5c6851af6ac2261dc1b6d49
Moliari <danielmoliaribarbosa@gmail.com>
2024-08-23 12:55:42 -0300

    fix: Tenta outro método para consertar e exclui o anterior

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Middleware\HandleInertiaRequests;
-use App\Http\Middleware\RedirectIfNotSecure; // Adicione isso
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,7 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
             HandleInertiaRequests::class,
-            RedirectIfNotSecure::class, // Adicione isso
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {

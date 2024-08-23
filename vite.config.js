588fa3323c9d4eaf656dc714c951471d167eb62f
Moliari <danielmoliaribarbosa@gmail.com>
2024-08-23 12:21:37 -0300

    fix: Testa um novo caminho para impedir que os arquivos fiquem com http

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "/build/", // Adicione a base URL para garantir que os ativos sejam carregados corretamente
+    base: "/", // Garante que os caminhos gerados são relativos ao domínio base
     plugins: [
         laravel({
             input: ["resources/js/app.jsx"],
